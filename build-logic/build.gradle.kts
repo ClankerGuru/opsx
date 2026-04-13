2db69fca2d495a8325a82005a18349d815e036a5
slop <slop@clanker.guru>
2026-04-13 15:58:47 -0700

    Bump vanniktech maven-publish to 0.36.0 to match srcx

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${embeddedKotlinVersion}")
-    implementation("com.vanniktech:gradle-maven-publish-plugin:0.32.0")
+    implementation("com.vanniktech:gradle-maven-publish-plugin:0.36.0")
     implementation("org.gradle.toolchains:foojay-resolver:1.0.0")
     implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.9.1")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.8")
